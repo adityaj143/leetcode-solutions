c2a11e8b7361c38cf9056e0f9b5156df5af12982
Aditya Vijay Jambhulkar <146608269+adityaj143@users.noreply.github.com>
2026-08-04 02:30:16 +0000

    Sync LeetCode submission Runtime - 20 ms (45.32%), Memory - 19.5 MB (42.45%)

---
+++ 0040-combination-sum-ii/solution.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def backtrack(start, path, total):
+            if total == target:
+                res.append(path[:])
+                return
+
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                # Skip duplicates
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                path.append(candidates[i])
+                backtrack(i + 1, path, total + candidates[i])  # Use each number once
+                path.pop()
+
+        backtrack(0, [], 0)
+        return res
