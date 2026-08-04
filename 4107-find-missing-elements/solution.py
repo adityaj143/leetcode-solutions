4ee0be7ee9d54513e13feff0a34a16f6bd846d42
Aditya Vijay Jambhulkar <146608269+adityaj143@users.noreply.github.com>
2026-08-04 02:23:03 +0000

    Sync LeetCode submission Runtime - 2 ms (66.71%), Memory - 19.3 MB (61.71%)

---
+++ 4107-find-missing-elements/solution.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        return [i for i in range(min(nums), max(nums) + 1) if i not in s]
