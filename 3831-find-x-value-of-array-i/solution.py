5b2e891fbe60748bb16bdef9fa27de778f5b0891
Aditya Vijay Jambhulkar <146608269+adityaj143@users.noreply.github.com>
2026-09-21 00:18:45 +0000

    Sync LeetCode submission Runtime - 291 ms (89.47%), Memory - 34.2 MB (69.74%)

---
+++ 3831-find-x-value-of-array-i/solution.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
+        result = [0] * k
+        dp = [0] * k
+
+        for num in nums:
+            new_dp = [0] * k
+
+            
+            r = num % k
+            new_dp[r] += 1
+
+      
+            for old_r in range(k):
+                if dp[old_r] > 0:
+                    new_r = (old_r * r) % k
+                    new_dp[new_r] += dp[old_r]
+
+            dp = new_dp
+
+            for r in range(k):
+                result[r] += dp[r]
+
+        return result
